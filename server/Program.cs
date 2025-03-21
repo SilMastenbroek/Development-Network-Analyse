60d6adf3d62369227d05800687f3afb6a6ad138d
Sil <s.mastenbroek5@gmail.com>
2025-03-21 12:23:39 +0100

    Work in progress server

--- server/Program.cs
+++ server/Program.cs
@@ -20,12 +20,72 @@ class Program
 
 class ServerUDP
 {
+    private Socket serverSocket;
+    private EndPoint remoteEndpoint;
+    private const int port = 11000;
 
     //TODO: implement all necessary logic to create sockets and handle incoming messages
     // Do not put all the logic into one method. Create multiple methods to handle different tasks.
     public void start()
     {
+        System.Console.WriteLine("Server word opgestart...");
 
+        InitializeSocket();
+        System.Console.WriteLine("Server luistert op poort " + port);
+
+        // 1 keer Hello afhandelen (je gaat dit later in een loop zetten)
+        Message message = ReceiveMessage();
+        if (message.MsgType == MessageType.Hello)
+            HandleHello(message);
+        else
+            System.Console.WriteLine("Verwachtte Hello, maar kreeg iets anders: " + message.MsgType);
+
+        System.Console.WriteLine("Server stopt (alleen voor de test).");
+    }
+
+    private void InitializeSocket()
+    {
+        serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, port);
+        serverSocket.Bind(localEndPoint);
+
+        remoteEndpoint = new IPEndPoint(IPAddress.Any, 0); // Vult zichzelf in bij ReceiveForm
+    }
+
+    private Message ReceiveMessage()
+    {
+        byte[] buffer = new byte[4096];
+        int received = serverSocket.ReceiveFrom(buffer, ref remoteEndpoint);
+        string jsonMessage = Encoding.UTF8.GetString(buffer, 0, received);
+
+        System.Console.WriteLine("Bericht ontvangen: ");
+        System.Console.WriteLine(jsonMessage);
+
+        Message? message = JsonSerializer.Deserialize<Message>(jsonMessage);
+        return message ?? throw new Exception("Kon bericht niet parsen.");
+    }
+
+    private void HandleHello(Message message)
+    {
+        System.Console.WriteLine("Hello ontvangen met MsgId " + message.MsgId);
+
+        SendWelcome(message.MsgId + 1);
+    }
+
+    private void SendWelcome(int replyMsgId)
+    {
+        Message welcome = new Message
+        {
+            MsgId = replyMsgId,
+            MsgType = MessageType.Welcome,
+            Content = "Welkom van de server!"
+        };
+
+        string json = JsonSerializer.Serialize(welcome);
+        byte[] data = Encoding.UTF8.GetBytes(json);
+        serverSocket.SendTo(data, remoteEndpoint);
+
+        System.Console.WriteLine("Welkom verzonden!");
     }
 
     //TODO: create all needed objects for your sockets 
