7a3168ed4a1f3633e3324d0eada10a715026a0f3
Sil <s.mastenbroek5@gmail.com>
2025-06-09 16:41:42 +0200

    W.I.P.

--- client/Program.cs
+++ client/Program.cs
@@ -37,21 +37,68 @@ class ClientUDP
 
     public static void start()
     {
-
         //TODO: [Create endpoints and socket]
-
+        IPEndPoint serverEndpoint = new IPEndPoint(IPAddress.Parse(setting.ServerIPAddress!), setting.ServerPortNumber);
+        Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
 
         //TODO: [Create and send HELLO]
+        Message helloMessage = new Message { MsgId = 1, MsgType = MessageType.Hello, Content = "Hello from client" };
+        SendMessage(clientSocket, serverEndpoint, helloMessage);
 
         //TODO: [Receive and print Welcome from server]
+        ReceiveMessage(clientSocket);
+
+        DNSRecord[] lookups = new DNSRecord[] {
+            new DNSRecord { Type = "A", Name = "www.outlook.com" },
+            new DNSRecord { Type = "MX", Name = "example.com" },
+            new DNSRecord { Type = "A", Name = "no-such-domain.com" },
+            new DNSRecord { Type = "A", Name = "bad_domain.com" }
+        };
+
+        int msgIdCounter = 2;
+
+        foreach (var lookup in lookups)
+        {
+            Message lookupMessage = new Message { MsgId = msgIdCounter++, MsgType = MessageType.DNSLookup, Content = lookup };
+            SendMessage(clientSocket, serverEndpoint, lookupMessage);
+
+            var reply = ReceiveMessage(clientSocket);
+
+            if (reply.MsgType == MessageType.DNSLookupReply)
+            {
+                //TODO: [Send Acknowledgment to Server]
+                Message ackMessage = new Message { MsgId = msgIdCounter++, MsgType = MessageType.Ack, Content = reply.MsgId };
+                SendMessage(clientSocket, serverEndpoint, ackMessage);
+            }
+        }
+
+        // Wait for End Message
+        ReceiveMessage(clientSocket);
 
         // TODO: [Create and send DNSLookup Message]
-
+        static void SendMessage(Socket socket, IPEndPoint endpoint, Message message)
+        {
+            string json = JsonSerializer.Serialize(message);
+            byte[] buffer = Encoding.UTF8.GetBytes(json);
+            socket.SendTo(buffer, endpoint);
+            Console.WriteLine($"Sent: {json}");
+        }
 
         //TODO: [Receive and print DNSLookupReply from server]
+        static Message ReceiveMessage(Socket socket)
+        {
+            EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+            byte[] buffer = new byte[4096];
+            int received = socket.ReceiveFrom(buffer, ref remoteEP);
+            string json = Encoding.UTF8.GetString(buffer, 0, received);
+            Message? message = JsonSerializer.Deserialize<Message>(json);
+            Console.WriteLine($"Received: {json}");
+            return message;
+        }
 
-
-        //TODO: [Send Acknowledgment to Server]
+    
+    
+        
 
         // TODO: [Send next DNSLookup to server]
         // repeat the process until all DNSLoopkups (correct and incorrect onces) are sent to server and the replies with DNSLookupReply
--- server/Program.cs
+++ server/Program.cs
@@ -35,17 +35,49 @@ class ServerUDP
     static Setting? setting = JsonSerializer.Deserialize<Setting>(configContent);
 
     // TODO: [Read the JSON file and return the list of DNSRecords]
+    static DNSRecord[]? dNSRecords = JsonSerializer.Deserialize<DNSRecord[]>(File.ReadAllText(@"dns_records.json"));
 
-
+    // (zelf toegevoegd) Define the steps for the server state machine
+    enum ServerStep { AwaitHello, AwaitLookup, AwaitAck }
+    static ServerStep currentStep = ServerStep.AwaitHello;
+    static System.Timers.Timer inactivityTimer;
+    static int countdown;
 
 
     public static void start()
     {
-
-
         // TODO: [Create a socket and endpoints and bind it to the server IP address and port number]
 
+        // Maak een IPEndPoint-object voor het binden van de socket aan het IP-adres en de poort van de server.
+        IPEndPoint serverEndpoint = new IPEndPoint(IPAddress.Parse(setting.ServerIPAddress!), setting.ServerPortNumber);
 
+        // Creëer een UDP-socket die gebruikmaakt van IPv4 (AddressFamily.InterNetwork), UDP (SocketType.Dgram) transportlaag en het UDP-protocol.
+        Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+        // Bind de aangemaakte socket aan het opgegeven IP-adres en de poort zodat de server luisterend klaarstaat.
+        serverSocket.Bind(serverEndpoint);
+        Console.WriteLine($"Server listening on {serverEndpoint}");
+
+        // Maak een EndPoint-object aan dat wordt gebruikt om het IP-adres en de poort van de inkomende client-berichten te ontvangen.
+        EndPoint clientEP = new IPEndPoint(IPAddress.Any, 0);
+        byte[] buffer = new byte[4096];
+
+        inactivityTimer = new System.Timers.Timer(1000); // 1 second interval
+        inactivityTimer.Elapsed += (sender, e) =>
+        {
+            countdown--;
+            Console.WriteLine($"Time remaining: {countdown} seconds");
+            if (countdown <= 0)
+            {
+                Message endMessage = new Message { MsgId = 9999, MsgType = MessageType.End, Content = "End due to inactivity" };
+                SendMessage(serverSocket, endMessage, clientEP);
+                currentStep = ServerStep.AwaitHello;
+                Console.WriteLine("Inactivity timeout. Sent End message.");
+                inactivityTimer.Stop();
+            }
+        };
+
+        ResetTimer();
 
         // TODO:[Receive and print a received Message from the client]
 
@@ -76,6 +108,14 @@ class ServerUDP
 
         // TODO:[If no further requests receieved send End to the client]
 
+    }
+    
+    static void ResetTimer() // Zelf toegevoegd
+    {
+        countdown = 10;
+        inactivityTimer.Stop();
+        inactivityTimer.Start();
+        Console.WriteLine($"Timer reset to {countdown} seconds.");
     }
 
 
